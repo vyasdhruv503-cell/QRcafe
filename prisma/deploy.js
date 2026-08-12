17bcf7804287c0e44700c0616a3a882cb828f938
vyasdhruv503-cell <vyasdhruv503@gmail.com>
2026-08-12 16:06:22 +0530

    fix(db): use sslmode=no-verify for Aiven PostgreSQL URL to ensure seamless TLS handshake on Render

--- prisma/deploy.js
+++ prisma/deploy.js
@@ -6,7 +6,7 @@ const AIVEN_DB_URL = [
   'postgresql://avnadmin:',
   'AVNS_jkxy',
   'CM5b6pHT1PkTsQZ',
-  '@pg-e51004a-vyasdhruv503-34d1.f.aivencloud.com:17669/defaultdb?sslmode=require'
+  '@pg-e51004a-vyasdhruv503-34d1.f.aivencloud.com:17669/defaultdb?sslmode=no-verify'
 ].join('');
 
 let dbUrl = process.env.DATABASE_URL;
